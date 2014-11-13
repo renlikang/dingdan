d25ffa412117e19159f564f820350e9b87dba38b
renlikang <renlikang@163.com>
2014-11-13 16:46:27 +0800

    aaaa

--- app/models/Customer.php
+++ app/models/Customer.php
@@ -10,18 +10,16 @@ use Yii;
  * @property integer $id
  * @property string $customer_code
  * @property string $customer_name
- * @property string $link_man
- * @property string $logo_linkman
- * @property string $office_address
- * @property string $warehouse_address
- * @property string $qc_require
- * @property string $subscribe
- * @property string $odo
- * @property string $good_order
- * @property string $signature
- * @property string $relatived_inspe
- * @property string $order_limit
- * @property string $good_pay
+ * @property string $cw_csode
+ * @property string $shop_name
+ * @property string $souhuoren
+ * @property string $souhuo_address
+ * @property string $lianxifangshi
+ * @property string $customer_type
+ * @property integer $rebate_type
+ * @property integer $rebate
+ * @property string $account
+ * @property string $file_address
  * @property string $memo
  */
 class Customer extends \yii\db\ActiveRecord
@@ -40,12 +38,11 @@ class Customer extends \yii\db\ActiveRecord
     public function rules()
     {
         return [
-            [['customer_code', 'customer_name', 'link_man', 'logo_linkman', 'odo', 'good_order', 'signature', 'relatived_inspe', 'order_limit', 'good_pay','office_address', 'warehouse_address', 'subscribe'],'required','message'=>'{attribute}不能为空'],
-            [['customer_code', 'customer_name', 'link_man', 'logo_linkman', 'odo', 'good_order', 'signature', 'relatived_inspe', 'order_limit', 'good_pay'], 'string', 'max' => 64],
-            [['office_address', 'warehouse_address', 'qc_require', 'subscribe'], 'string', 'max' => 128],
-            [['memo'], 'string', 'max' => 255],
-            [['customer_code','customer_name'],'unique','message'=>'{attribute}已经存在']
-        ]; 
+            [['rebate_type', 'rebate'], 'integer'],
+            [['customer_code', 'cw_csode', 'souhuoren', 'account'], 'string', 'max' => 64],
+            [['customer_name', 'shop_name', 'souhuo_address', 'lianxifangshi', 'customer_type', 'file_address'], 'string', 'max' => 128],
+            [['memo'], 'string', 'max' => 1000]
+        ];
     }
 
     /**
@@ -54,22 +51,20 @@ class Customer extends \yii\db\ActiveRecord
     public function attributeLabels()
     {
         return [
-            'id'                => 'ID',
-            'customer_code'     => '客户代码',
-            'customer_name'     => '客户名称',
-            'link_man'          => '联系人',
-            'logo_linkman'      => '品牌负责人',
-            'office_address'    => '办公地址及寄票对账',
-            'warehouse_address' => '仓库地址及收货人信息',
-            'qc_require'        => '收货要求',
-            'subscribe'         => '预约',
-            'odo'               => '出库单',
-            'good_order'        => '订单',
-            'signature'         => '签章',
-            'relatived_inspe'   => '卫检',
-            'order_limit'       => '订单期限',
-            'good_pay'          => '货款及返利',
-            'memo'              => '备注',
+            'id' => 'ID',
+            'customer_code' => '客户代码',
+            'customer_name' => '客户名称',
+            'cw_csode' => '仓位代码',
+            'shop_name' => '门店名称',
+            'souhuoren' => '收货人',
+            'souhuo_address' => '收货地址',
+            'lianxifangshi' => '联系方式',
+            'customer_type' => '客户类型',
+            'rebate_type' => '返利类型',
+            'rebate' => '返利',
+            'account' => '汇款账号',
+            'file_address' => '文件寄送地址',
+            'memo' => '备注',
         ];
     }
 }
